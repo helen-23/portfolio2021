68d1142d0b02cf3b06e0e2d94e401a56ad700baa
helen-23 <helendai23@gmail.com>
2021-07-03 21:07:14 -0400

    Added programming projects section in home

--- application/urls.py
+++ application/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('FullMonteWeb', views.fullmonte_web, name='FullMonteWeb'),
+    path('PocketTravel', views.pocket_travel, name='PocketTravel'),
+    path('ReversiAI', views.reversi, name='ReversiAI'),
+    path('GunmanJump', views.gunman_jump, name='GunmanJump'),
+    path('BubbleMania', views.bubble_mania, name='BubbleMania'),
+    path('Cycle63', views.cycle_63, name='Cycle63'),
+    path('TunerPro', views.tuner_pro, name='TunerPro'),
+    path('OregonWildlifeClassifier', views.ml_classifier, name='OregonWildlifeClassifier')
 ]
--- application/views.py
+++ application/views.py
@@ -4,3 +4,27 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def fullmonte_web(request):
+    return render(request, "FullMonteWeb.html")
+
+def pocket_travel(request):
+    return render(request, "PocketTravel.html")
+
+def reversi(request):
+    return render(request, "ReversiAI.html")
+
+def gunman_jump(request):
+    return render(request, "GunmanJump.html")
+
+def bubble_mania(request):
+    return render(request, "BubbleMania.html")
+
+def cycle_63(request):
+    return render(request, "Cycle63.html")
+
+def tuner_pro(request):
+    return render(request, "TunerPro.html")
+
+def ml_classifier(request):
+    return render(request, "OregonWildlifeClassifier.html")
